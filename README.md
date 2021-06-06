c37e1b06e23c15c15fd7c0e2ead3598a18bdc8b1
Alex Christenson <christenson10@gmail.com>
2021-06-06 16:42:02 -0400

    moved readme

---
+++ README.md
@@ -0,0 +1,40 @@
+# Challenge 11 - Note Taker Application
+
+## Purpose
+As a small business owner, I want to be able to write and save notes to help organize and keep track of my tasks.
+
+## Built With
+* HTML/CSS Starter Code 
+* JavaScript
+* Inquirer
+* JSON
+* Node
+* API Routes
+* Express.js
+
+## Requirements/Notes
+* AS A small business owner
+* I WANT to be able to write and save notes
+* SO THAT I can organize my thoughts and keep track of tasks I need to complete
+
+## Live URL
+
+## Github Link
+https://christenson10.github.io/challenge11/
+
+## Pictures
+
+## README Preview 1
+
+
+![Screenshot1](NoteTakerScreenshot1.jpg)
+
+## README Preview 2
+
+
+![Screenshot2](NoteTakerScreenshot2.jpg)
+
+## README Preview 3
+
+
+![Screenshot2](NoteTakerScreenshot3.jpg)
---
+++ miniature-eureka/Develop/server.js
@@ -0,0 +1,34 @@
+const fs = require('fs');
+const express = require('express');
+
+const path = require('path');
+const router = require('express').Router();
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
+
+app.get('/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, '/public/notes.html'));
+});
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/index.html'));
+  });
+
+// app.get('/api/notes', (req, res) => {
+// //use FS library to read db.json file in db folder... take data to send back 
+//   // data getNotes().then((notes) => res.json(results);
+// });
+
+// app.post('/api/notes', (req, res) => {
+// //user saves new note into database...
+//   // res.json(createNote);
+// });
+  
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}!`);
+  });
